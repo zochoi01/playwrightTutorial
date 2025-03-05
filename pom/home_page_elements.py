b65ba320796a217b63d0661a92d5b0c7074fb0dc
dattran <dat.tran@creqavn.net>
2025-03-05 23:25:42 +0700

    First commit

---
+++ pom/home_page_elements.py
@@ -0,0 +1,8 @@
+class Homepage:
+    log_in = "//a[.='Đăng Nhập']"
+    search_bar = ".shopee-searchbar-input"
+    logo = "//div[@class='login_logo']"
+    user_name_input = "//input[@id='user-name']"
+    pass_word_input = "//input[@id='password']"
+    log_in_submit = "//input[@id='login-button']"
+    cart = ".shopping_cart_link"
